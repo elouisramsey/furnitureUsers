b500d919e0f8d1d943f24bdcc07561a9ede6b9b3
louis <madelouisramsey@gmail.com>
2021-04-21 08:08:44 +0100

    cors again

--- server.js
+++ server.js
@@ -14,15 +14,11 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(
-  cors({
-    allowedHeaders: ['sessionId', 'Content-Type'],
-    exposedHeaders: ['sessionId'],
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false
-  })
-)
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
+})
 
 // Connect DB
 mongoose
